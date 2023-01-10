53c4d10193c7ff97a7c530b0c098ca411c7271d2
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-01-10 15:48:04 +0600

    express schema define

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
+const { graphqlHTTP } = require('express-graphql');
+const schema =require('./schema/schema')
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql:true
+}))
+
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`server is running at ${port}`);
+})
---
+++ schema/schema.js
@@ -0,0 +1,29 @@
+const { projects, clients } = require('./data');
+const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema } = require('graphql');   
+
+const ClientType = new GraphQLObjectType({
+    name: "Client",
+    fields: () => {
+        id: { type: GraphQLID }
+        name:{type:GraphQLString}
+        email:{type:GraphQLString}
+        phone:{type:GraphQLString}
+    }
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: "RootQuery",
+    fields: {
+        client: {
+            type: ClientType,
+            args: { id: { type: GraphQLID } },
+            resolve(parent, args){
+                return clients.find(client => client.id === args.id);
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query:RootQuery
+})
