926a92fdc81de85aee13892667fa25717502ea5a
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-01-12 14:55:56 +0600

    specified the query type

--- schema/schema.js
+++ schema/schema.js
@@ -1,140 +1,201 @@
-const { projects, clients } = require('./data');
-const User = require('../models/User');
-const Course = require('../models/Course');
 
-const { GraphQLObjectType, GraphQLID, GraphQLString, GraphQLSchema, GraphQLList, GraphQLNonNull, GraphQLEnumType } = require('graphql');   
+const User = require("../models/User");
+const Course = require("../models/Course");
+
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLEnumType,
+} = require("graphql");
 
 const UserType = new GraphQLObjectType({
-    name: "User",
-    fields: () => ({
-        id: { type: GraphQLID },
-        name:{type:GraphQLString},
-        email:{type:GraphQLString},
-        phone:{type:GraphQLString}
-    })
-})
+  name: "User",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
 
 const CourseType = new GraphQLObjectType({
-    name: "Course",
-    fields: () => ({
-        id: { type: GraphQLID },
-        name:{type:GraphQLString},
-        description:{type:GraphQLString},
-        status: { type: GraphQLString },
-        user: {
-            type: UserType,
-            resolve(parent, args) {
-                return User.findById(parent.userId)
-            }
-        }
-       
-    })
-})
+  name: "Course",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    user: {
+      type: UserType,
+      resolve(parent, args) {
+        return User.findById(parent.userId);
+      },
+    },
+  }),
+});
 
 const RootQuery = new GraphQLObjectType({
-    name: "RootQuery",
+  name: "RootQuery",
     fields: {
-        courses: {
-            type: GraphQLList(CourseType),
-            resolve() {
-                return Course.find();
-            }
-        },
-        course: {
-            type: CourseType,
-            args: { id: { type: GraphQLID } },
-            resolve(parent, args){
-                return Course.findById(args.id)
-            }
-        },
-        users: {
-            type: GraphQLList(UserType),
-            resolve() {
-                return User.find();
-            }
-        },
-        user: {
-            type: UserType,
-            args: { id: { type: GraphQLID } },
-            resolve(parent, args){
-                return User.findById(args.id);
-            }
-        }
-    }
-})
+    //   all course finding query
+    courses: {
+      type: GraphQLList(CourseType),
+      resolve() {
+        return Course.find();
+      },
+    },
+    course: {
+      type: CourseType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Course.findById(args.id);
+      },
+    },
+    users: {
+      type: GraphQLList(UserType),
+      resolve() {
+        return User.find();
+      },
+    },
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return User.findById(args.id);
+      },
+    },
+  },
+});
 
 const mutation = new GraphQLObjectType({
-    name: "Mutations",
-    fields: {
-        // add user
-        addUser: {
-            type: UserType,
-            args: {
-                name: { type: GraphQLNonNull(GraphQLString) },
-                email: { type: GraphQLNonNull(GraphQLString) },
-                phone: { type: GraphQLNonNull(GraphQLString) },
+  name: "Mutations",
+  fields: {
+    // add user
+    addUser: {
+      type: UserType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        email: { type: GraphQLNonNull(GraphQLString) },
+        phone: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        const user = new User({
+          name: args.name,
+          email: args.email,
+          phone: args.phone,
+        });
+        return user.save();
+      },
+    },
+    // delete user
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        return User.findByIdAndDelete(args.id);
+      },
+    },
+    updateUser: {
+      type: UserType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLString) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        email: { type: GraphQLNonNull(GraphQLString) },
+        phone: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        return User.findByIdAndUpdate(
+          args.id,
+          {
+            $set: {
+              name: args.name,
+              email: args.email,
+              phone: args.phone,
             },
-            resolve(parent, args) {
-                const user = new User({
-                    name: args.name,
-                    email: args.email,
-                    phone:args.phone
-                })
-                return user.save()
-            }
-        },
-        // delete user
-        deleteUser: {
-            type: UserType,
-            args: {
-                id: { type: GraphQLNonNull(GraphQLString) }
+          },
+          { new: true }
+        );
+      },
+    },
+    // add course
+    addCourse: {
+      type: CourseType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        description: { type: GraphQLNonNull(GraphQLString) },
+        status: {
+          type: new GraphQLEnumType({
+            name: "CourseStatus",
+            values: {
+              Good: { value: "Good" },
+              Better: { value: "Better" },
+              Best: { value: "Best" },
             },
-            resolve(parent, args) {
-                return User.findByIdAndDelete(args.id)
-            }
+          }),
+          defaultValue: "Good",
         },
-        // add course
-        addCourse: {
-            type: CourseType,
-            args: {
-                name: { type: GraphQLNonNull(GraphQLString) },
-                description: { type: GraphQLNonNull(GraphQLString) },
-                status: {
-                    type: new GraphQLEnumType({
-                        name: "CourseStatus",
-                        values: {
-                            "Good":{value:"Good"},
-                            "Better":{value:"Better"},
-                            "Best":{value:"Best"}
-                        }
-                    }),
-                    defaultValue: "Good",
-                },
-                userId:{type:GraphQLNonNull(GraphQLID)}
+        userId: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        const course = new Course({
+          name: args.name,
+          description: args.description,
+          status: args.status,
+          userId: args.userId,
+        });
+        return course.save();
+      },
+    },
+    // delete course
+    deleteCourse: {
+      type: CourseType,
+      args: { id: { type: GraphQLNonNull(GraphQLString) } },
+      resolve(parent, args) {
+        return Course.findByIdAndDelete(args.id);
+      },
+    },
+    // update a course
+    updateCourse: {
+      type: CourseType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+        name: { type: GraphQLNonNull(GraphQLString) },
+        description: { type: GraphQLNonNull(GraphQLString) },
+        status: {
+          type: new GraphQLEnumType({
+            name: "CourseStatusUpdate",
+            values: {
+              Good: { value: "Good" },
+              Better: { value: "Better" },
+              Best: { value: "Best" },
             },
-            resolve(parent, args) {
-                const course = new Course({
-                    name: args.name,
-                    description: args.description,
-                    status: args.status,
-                    userId:args.userId
-                })
-                return course.save()
-            }
+          }),
         },
-        // delete course
-        deleteCourse: {
-            type: CourseType,
-            args: { id: { type: GraphQLNonNull(GraphQLString) } },
-            resolve(parent, args) {
-                return Course.findByIdAndDelete(args.id);
-            }
-        }
-        
-    }
-})
+      },
+      resolve(parent, args) {
+        return Course.findByIdAndUpdate(
+          args.id,
+          {
+            $set: {
+              name: args.name,
+              description: args.description,
+              status: args.status,
+            },
+          },
+          { new: true }
+        );
+      },
+    },
+  },
+});
 
 module.exports = new GraphQLSchema({
-    query: RootQuery,
-    mutation
-})
+  query: RootQuery,
+  mutation,
+});
